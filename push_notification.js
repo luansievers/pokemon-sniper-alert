f03179df60c7ee5b4e876ea15caefc70967c0cae
Luan Felipe Sievers <luan.sievers@gmail.com>
2019-04-15 19:25:50 -0300

    Update push_notification.js

--- push_notification.js
+++ push_notification.js
@@ -5,7 +5,6 @@ const TelegramBot = require(`node-telegram-bot-api`)
 
 const TOKEN = '720740691:AAHqaJ2HaBXiPKxtTKqOhNro8_LweqvOfDc'
 const ID_CHANNEL_NOTIFICATION = '259536527221063683'
-const ID_CHANNEL_PROMO = '294605545464135690'
 const CHAT_ID = '836091152'
 
 const client = new Discord.Client();
@@ -35,12 +34,6 @@ client.on('message', msg => {
         console.log(`${nome} - ${info}`)
         console.log(link)
 
-    } else if (msg.channel.id === ID_CHANNEL_PROMO && msg.content.includes("Random Answer set")) {
-        bot.sendMessage(CHAT_ID, `Random Answer set!`);
-    } else if (msg.channel.id === ID_CHANNEL_PROMO && msg.content.includes("GO GO GO!")) {
-        bot.sendMessage(CHAT_ID, `Giveaway started!`);
-    } else if (msg.channel.id === ID_CHANNEL_PROMO && msg.content.includes("Competition is finished, come back soon!")) {
-        bot.sendMessage(CHAT_ID, `Giveaway ended!`);
     }
 });
 
